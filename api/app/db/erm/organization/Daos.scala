dc00a2995fed1866f8fa3135ed6374375d18b725
Michael Bryzek <mike@flow.io>
2019-11-29 12:58:50 -0500

    Remove singleton

--- api/app/db/erm/organization/Daos.scala
+++ api/app/db/erm/organization/Daos.scala
@@ -4,7 +4,6 @@ import io.flow.organization.event.v0.models.OrganizationUpsertedV2
 import io.flow.common.v0.models._
 import io.flow.common.v0.models.json._
 import io.flow.event.relation.mapper.dao.{DaoArguments, Dao}
-import javax.inject.{Inject, Singleton}
+import javax.inject.Inject
 
-@Singleton
 class OrganizationsDao @Inject()(args: DaoArguments) extends Dao[OrganizationUpsertedV2, Organization](args)
