280aae7d5f560e681f1d88ca6b9c89fac9990e62
Michael Bryzek <mike@flow.io>
2020-01-04 23:02:17 -0500

    update

--- api/test/helpers/erm/ErmHelpers.scala
+++ api/test/helpers/erm/ErmHelpers.scala
@@ -2,18 +2,10 @@ package helpers.erm
 
 import io.flow.event.relation.mapper.ErmQueue
 import io.flow.test.utils.FlowPlaySpec
-import org.scalatest.concurrent.Eventually.{eventually, timeout}
-import org.scalatest.time.{Seconds, Span}
 
 trait ErmHelpers {
   self: FlowPlaySpec =>
 
   def ermQueue: ErmQueue = app.injector.instanceOf[ErmQueue]
-
-  def eventuallyInNSeconds[T](n: Long = 3)(f: => T): T = {
-    eventually(timeout(Span(n, Seconds))) {
-      f
-    }
-  }
 
 }
